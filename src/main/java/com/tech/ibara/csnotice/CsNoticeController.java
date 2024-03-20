9089f68ae326dbaf020ac3210072fff5111a8654
bboLightyear <158018897+bboLightyear@users.noreply.github.com>
2024-03-20 13:19:42 +0900

    ✨ bogeun feat cshome

--- src/main/java/com/tech/ibara/csnotice/CsNoticeController.java
+++ src/main/java/com/tech/ibara/csnotice/CsNoticeController.java
@@ -1,5 +1,14 @@
 package com.tech.ibara.csnotice;
 
-public class CsNoticeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class CsNoticeController {
+	
+	@RequestMapping("/cshome")
+	public String cshome() {
+		
+		return "csnotice/cshome";
+	}
 }
