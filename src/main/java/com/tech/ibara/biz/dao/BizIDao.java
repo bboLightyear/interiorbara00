96f3626aa0084745ac6355bddd4f3fd6b62e04b9
imyoursimi <imyoursimi@gmail.com>
2024-03-26 11:36:30 +0900

    ✨ sungmin feat biz_cases board

--- src/main/java/com/tech/ibara/biz/dao/BizIDao.java
+++ src/main/java/com/tech/ibara/biz/dao/BizIDao.java
@@ -2,11 +2,15 @@ package com.tech.ibara.biz.dao;
 
 import java.util.ArrayList;
 
+import com.tech.ibara.biz.dto.BizCasesDto;
+import com.tech.ibara.biz.dto.BizCasesImgDto;
 import com.tech.ibara.biz.dto.BizMgzDto;
 import com.tech.ibara.biz.dto.BizMgzImgDto;
 
 public interface BizIDao {
 
+//	매거진 게시판 쿼리
+	
 	public ArrayList<BizMgzDto> bizMgzList(int rowStart, int rowEnd, String searchKeyword, String selNum);
 
 	public BizMgzDto bizMgzContentView(String bm_no);
@@ -37,6 +41,21 @@ public interface BizIDao {
 	
 	public ArrayList<BizMgzImgDto> selectBizMgzImg(String bm_no);
 
+//	시공사례 게시판 쿼리
+	
+	public ArrayList<BizCasesDto> bizCasesList(int rowStart, int rowEnd, String searchKeyword, String string);
+
+	public void bizCasesWrite(String bc_title, String bc_content, String bc_writer, String user_idno);
+
+	public int selBc_no();
+
+	public void bizCasesImgWrite(int bc_no, String originFile, String changeFile);
+
+	public void bizCasesUpHit(String bc_no);
+
+	public BizCasesDto bizCasesContentView(String bc_no);
+
+	public ArrayList<BizCasesImgDto> selectBizCasesImg(String bc_no);
 
 
 
